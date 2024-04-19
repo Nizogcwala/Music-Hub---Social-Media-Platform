005c7556c7b2ec5891a13f0c12a3c89bbd56c2df
Nizogcwala <nizogasa@gmail.com>
2024-04-19 13:58:26 +0000

    Modified script.js

---
+++ MusicHubApp/static/js/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    // Add JavaScript functionality here
+    console.log('Script loaded');
+});
